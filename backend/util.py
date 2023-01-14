c3e1ed3e081d2ff313d085456ca96a9463f2e50c
Chen YiJia <65040076+chence08@users.noreply.github.com>
2023-01-14 15:52:07 +0800

    getArrivalTime and .env

--- backend/util.py
+++ backend/util.py
@@ -1,16 +1,37 @@
 import requests
-from geopy import distance
+from geopy.distance import distance
 import json
+from dotenv import load_dotenv
+import os
 
 def getClosestBusStop(latlon = (1.30637, 103.77337)):
     with open('BusStops.json', 'r') as f:
         data = json.load(f)
     bs = [
         (
-            distance.distance((x['Latitude'], x['Longitude']), latlon).m,
+            distance((x['Latitude'], x['Longitude']), latlon).m,
             x['BusStopCode']
         )
         for x in data
     ]
     bs.sort()
     return bs[0]
+
+def getArrivalTime(busStop = '17099') -> list:
+    """_summary_
+
+    Args:
+        busStop (str, optional): code. Defaults to '17099'.
+
+    Returns:
+        list: list of arriving buses
+    """
+    load_dotenv()
+    headers = {
+        "AccountKey": os.getenv("DATAMALL_API_KEY"),
+        "accept": "application/json"
+    }
+    url = 'http://datamall2.mytransport.sg/ltaodataservice/BusArrivalv2'
+    params = {"BusStopCode": busStop}
+    content = requests.get(url, headers=headers, params=params).json()["Services"]
+    return content
