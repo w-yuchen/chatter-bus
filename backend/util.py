812e8d73ac192109de582aa2b05fa63777a4c51a
Chen YiJia <65040076+chence08@users.noreply.github.com>
2023-01-14 15:21:07 +0800

    finished getClosestBusStop function

---
+++ backend/util.py
@@ -0,0 +1,16 @@
+import requests
+from geopy import distance
+import json
+
+def getClosestBusStop(latlon = (1.30637, 103.77337)):
+    with open('BusStops.json', 'r') as f:
+        data = json.load(f)
+    bs = [
+        (
+            distance.distance((x['Latitude'], x['Longitude']), latlon).m,
+            int(x['BusStopCode'])
+        )
+        for x in data
+    ]
+    bs.sort()
+    return bs[0]
