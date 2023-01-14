1b1eb90e7b17ba3f040c9fd948687c9a3a7027d0
Joshua Chew Jian Xiang <59608088+josuaaah@users.noreply.github.com>
2023-01-14 15:35:27 +0800

    Implement frontend for status updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
+import ListGroup from "react-bootstrap/ListGroup";
+import Row from "react-bootstrap/Row";
+
+// import React from 'react';
+
+const header = (
+  <header className="App-header">
+    <span className="App-title px-4">Bus Buddies</span>
+    <span className="App-subtitle">
+      See how your bus buddies are doing in life.
+    </span>
+  </header>
+);
+
+const section = (title: string, children: any) => (
+  <div>
+    <div className="section-title">{title}</div>
+    <Container className="shadow-lg rounded py-3">{children}</Container>
+  </div>
+);
+
+const buddies = [
+  { username: "chence08", status: "yo @josuaaah i am doing some math" },
+  { username: "wangyuchen", status: "trying to figure out RestAPI" },
+  { username: "some-old-guy", status: "Where am I? What bus is this? This bus go Jurong Point one right" },
+];
+
+const buddyDisplay = (
+  <ListGroup variant="flush">
+    {buddies.map(({ username, status }) => (
+      <ListGroup.Item>
+        <div><b>{username}</b></div>
+        <div className="font-italic">{status}</div>
+      </ListGroup.Item>
+    ))}
+  </ListGroup>
+);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {header}
+
+      <Container fluid className="p-4">
+        <Row>
+          <Col xs={6}>
+            {section(
+              "My location",
+              <div>
+                You are on bus <b>187</b>, heading towards <b>Pulau Tekong</b>{" "}
+                because you are a chao recruit.
+              </div>
+            )}
+            <br></br>
+            {section(
+              "My display",
+              <div>
+                <InputGroup size="lg" className="mb-3">
+                  <InputGroup.Text id="basic-addon1">@</InputGroup.Text>
+                  <Form.Control
+                    placeholder="I am called..."
+                    aria-label="Username"
+                    aria-describedby="basic-addon1"
+                  />
+                </InputGroup>
+                <InputGroup>
+                  <InputGroup.Text>How are you</InputGroup.Text>
+                  <Form.Control as="textarea" aria-label="With textarea" />
+                </InputGroup>
+              </div>
+            )}
+          </Col>
+          <Col>{section("My Bus Buddies", <div>{buddyDisplay}</div>)}</Col>
+        </Row>
+      </Container>
     </div>
   );
 }
